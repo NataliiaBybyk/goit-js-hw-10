b3206e7440fec6d63ee9aad5e47ec2c735233213
NataliiaBybyk <natashakorobochka@gmail.com>
2025-09-17 20:34:19 +0200

    initial

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,38 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const stateValue = form.state.value;
+  const delayInput = form.delay.value;
+
+  const makePromise = delay => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (stateValue === 'fulfilled') {
+          resolve(`✅ Fulfilled promise in ${delay}ms`);
+        } else reject(`❌ Rejected promise in ${delay}ms`);
+      }, delay);
+    });
+  };
+
+  makePromise(delayInput)
+    .then(value => {
+      iziToast.success({
+        title: value,
+        position: 'topRight',
+      });
+    })
+    .catch(error =>
+      iziToast.error({
+        title: error,
+        position: 'topRight',
+      })
+    );
+  form.reset();
+});
